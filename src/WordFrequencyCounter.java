baa837db28b3f19ca2cfa72416038ff94fae275c
Ozge Ozer <104353749+ozgeozerr@users.noreply.github.com>
2023-12-07 15:03:40 +0300

    basic word frequency counter using HashMap

---
+++ src/WordFrequencyCounter.java
@@ -0,0 +1,86 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+ public class WordFrequencyCounter {
+     public static void main(String[]args){
+
+         Scanner scanner = new Scanner(System.in);
+
+         System.out.println("Welcome to Word Frequency Counter. Please enter a sentence: ");
+
+         String sentence = scanner.nextLine();
+
+         //now, I will create a Hashmap to store the word frequencies
+
+         Map<String,Integer> wordfreqmap = new HashMap<>();
+
+         //now to seperate the words, I will use the split method
+
+         String[] words = sentence.split("\\s+");
+
+         //counting part for the frequency
+
+         for(String word : words){
+
+             String lowercaseWord = word.toLowerCase();
+
+             //is it already in the map?
+             if (wordfreqmap.containsKey(lowercaseWord)) {
+
+                 //yes
+                 wordfreqmap.put(lowercaseWord, wordfreqmap.get(lowercaseWord) + 1);
+             } else {
+                 //no
+                 wordfreqmap.put(lowercaseWord,1);
+
+             }
+
+
+         }
+
+         //now let's print the frequencies
+
+         System.out.println("word frequences: ");
+
+         System.out.println(wordfreqmap.entrySet());
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
